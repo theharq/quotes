558800a84d1cf13949e99fe50533424db28894da
Hildebrando Rueda <hildebrando.rueda@gmail.com>
2016-11-29 13:48:01 -0500

    Refresh quote every 10 minutes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,9 @@
 class QuotesController < ApplicationController
   def index
     @quote = Quote.order("random()").first
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :quotes, only: :index
   root "quotes#index"
 end
